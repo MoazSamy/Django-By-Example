a66a16d63bb698564b6239d7c4c71e228f445c0c
Moaz Samy <moazsamy333@gmail.com>
2022-12-27 14:06:24 +0200

    Adding dashboard url

--- Social site project/account/urls.py
+++ Social site project/account/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # This is the function defined view
     # path("login/", views.user_login, name="login"),
     # These are the CBV from django
+    path("", views.dashboard, name="dashboard"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
