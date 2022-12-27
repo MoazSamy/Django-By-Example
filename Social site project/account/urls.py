9cf92b166e15b23e7b12b3cacffbef910cfe63af
Moaz Samy <moazsamy333@gmail.com>
2022-12-27 17:16:06 +0200

    Adding password reset urls.

--- Social site project/account/urls.py
+++ Social site project/account/urls.py
@@ -5,10 +5,14 @@ from . import views
 urlpatterns = [
     # This is the function defined view
     # path("login/", views.user_login, name="login"),
-    # These are the CBV from django
+
+
+    # These are the CBV from django for login urls
     path("", views.dashboard, name="dashboard"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+
     # Change password urls
     path(
         "password-change/",
@@ -19,5 +23,25 @@ urlpatterns = [
         "password-change/done/",
         auth_views.PasswordChangeDoneView.as_view(),
         name="password_change_done",
+    ),
+
+    # Password reset urls
+     path('password-reset/',
+          auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
     ),
 ]
