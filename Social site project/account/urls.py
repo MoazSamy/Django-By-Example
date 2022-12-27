2e90d9cbc1c9c2f5a090671a73cf0fe4aa7c8328
Moaz Samy <moazsamy333@gmail.com>
2022-12-27 13:35:24 +0200

    Swapped to CBV instead of FBV

--- Social site project/account/urls.py
+++ Social site project/account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("login/", views.user_login, name="login"),
+    # This is the function defined view
+    # path("login/", views.user_login, name="login"),
+    # These are the CBV from django
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
