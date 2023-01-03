c8ee2b1c0545c82f4a64e060633534b399744d58
Moaz Samy <moazsamy333@gmail.com>
2023-01-03 15:37:49 +0200

    Adding action creation on register, image like, create and user follow

--- Social site project/account/views.py
+++ Social site project/account/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from actions.utils import create_action
+from actions.models import Action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 
@@ -32,7 +34,14 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list("id", flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile').prefetch_related('target')[:10]
+    return render(
+        request, "account/dashboard.html", {"section": "dashboard", "actions": actions}
+    )
 
 
 @login_required
@@ -66,6 +75,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, "has created an account.")
             return render(request, "account/register_done.html", {"new_user": new_user})
 
     else:
@@ -88,22 +98,21 @@ def user_detail(request, username):
         request, "account/user/detail.html", {"section": "people", "user": user}
     )
 
+
 @require_POST
 @login_required
 def user_follow(request):
-    user_id = request.POST.get('id')
-    action = request.POST.get('action')
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
     if user_id and action:
         try:
             user = User.objects.get(id=user_id)
-            if action=='follow':
-                Contact.objects.get_or_create(
-                    user_from = request.user,
-                    user_to = user
-                )
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, "is following", user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
-            return JsonResponse({'status':'ok'})
+            return JsonResponse({"status": "ok"})
         except User.DoesNotExist:
-            return JsonResponse({'status':'error'})
-    return JsonResponse({'status':'error'})
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
