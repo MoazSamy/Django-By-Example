5cf730c808f3ebe11d8875ec56465a50551624e3
Moaz Samy <moazsamy333@gmail.com>
2023-01-18 13:28:57 +0200

    Set stripe_id field using webhook

--- Online Shop project/payment/webhooks.py
+++ Online Shop project/payment/webhooks.py
@@ -15,13 +15,13 @@ def stripe_webhook(request):
         event = stripe.Webhook.construct_event(
             payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
         )
-    
+
     except ValueError as e:
         return HttpResponse(status=400)
-    
+
     except stripe.error.SignatureVerificationError as e:
         return HttpResponse(status=400)
-    
+
     if event.type == "checkout.session.completed":
         session = event.data.object
         if session.mode == "payment" and session.payment_status == "paid":
@@ -29,7 +29,10 @@ def stripe_webhook(request):
                 order = Order.objects.get(id=session.client_reference_id)
             except Order.DoesNotExist:
                 return HttpResponse(status=404)
+            # set paid field to True
             order.paid = True
+            # store Stripe payment ID
+            order.stripe_id = session.payment_intent
             order.save()
 
     return HttpResponse(status=200)
