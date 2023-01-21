38c82f46c49fb516c22902ffa250a439cb024c04
Moaz Samy <moazsamy333@gmail.com>
2023-01-21 09:12:17 +0200

    Limiting content type choices

--- E-learning platform project/courses/models.py
+++ E-learning platform project/courses/models.py
@@ -50,7 +50,11 @@ class Content(models.Model):
     module = models.ForeignKey(
         Module, related_name="contents", on_delete=models.CASCADE
     )
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={"model__in": ("text", "file", "image", "video")},
+    )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
 
