d4e65bb7509098dd5366c120a62deb055522d0b5
Moaz Samy <moazsamy333@gmail.com>
2023-01-23 21:39:23 +0200

    Pre-site caching

--- E-learning platform project/educa/settings.py
+++ E-learning platform project/educa/settings.py
@@ -49,7 +49,9 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.cache.UpdateCacheMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.cache.FetchFromCacheMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -141,6 +143,11 @@ CACHES = {
     }
 }
 
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 60*15
+CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+# Debug toolbar
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
