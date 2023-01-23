597bf0c9608a18c725a5b424d63d637a86c4031f
Moaz Samy <moazsamy333@gmail.com>
2023-01-23 16:21:19 +0200

    Relationship between students and courses

--- E-learning platform project/courses/models.py
+++ E-learning platform project/courses/models.py
@@ -23,6 +23,7 @@ class Course(models.Model):
     subject = models.ForeignKey(
         Subject, related_name="courses", on_delete=models.CASCADE
     )
+    students = models.ManyToManyField(User, related_name='courses_joined', blank=True)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50, unique=True)
     overview = models.TextField()
